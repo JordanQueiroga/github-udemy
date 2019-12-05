4c9e5833dc81bdf00978406138c68718abc5e386
Jordan <jordanpqueiroga@gmail.com>
2019-12-04 23:52:47 -0300

    Aula 47: Primeiro commit e marcação do meu app

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+
+      <div className="search">
+        <input type="search" placeholder="Digite o nome do usuário no GitHub" />
+      </div>
+
+      <div className="user-info">
+        <img src="https://avatars1.githubusercontent.com/u/22600632?v=4" />
+        <h1>
+          <a href="https://github.com/JordanQueiroga">Jordan Queiroga</a>
+        </h1>
+        <div className="repos-info">
+          <ul>
+            <li>- Repositórios: 122</li>
+            <li>- Seguidores: 10</li>
+            <li>- Seguindo: 10</li>
+          </ul>
+        </div>
+
+        <div className="actions">
+          <button>Ver repositórios</button>
+          <button>Ver favoritos</button>
+        </div>
+      </div>
+
+      <div className="repos">
+        <h2>Repositórios:</h2>
+        <ul>
+          <li><a href="#">Nome do repositório</a></li>
+        </ul>
+
+        <div className="starred">
+          <h2>Favoritos:</h2>
+          <ul>
+            <li><a href="#">Nome repositório</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;

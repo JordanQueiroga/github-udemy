cda4ae4d4500417b277f2b4d4797a65c70854e7d
=Jordan Queiroga <jordanpqueiroga@gmail.com>
2020-01-11 19:40:38 -0300

    Modulo 01 - Parte 2: Aula 52 => Consumindo API do GitHub

--- src/componets/Search.js
+++ src/componets/Search.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import ajax from "@fdaciuk/ajax";
 
 const Search = () => (
     <div className="search">
-        <input type="search" placeholder="Digite o nome do usuário no GitHub" />
+        <input type="search" placeholder="Digite o nome do usuário no GitHub"
+            onKeyUp={(e) => {
+                const keyCode = e.which || e.keyCode;
+                const ENTER = 13;
+                const value = e.target.value
+                if (keyCode == ENTER) {
+                    ajax().get(`https://api.github.com/users/${value}`)
+                    .then((result)=>{
+                        console.log(result)
+                    })
+                }
+            }}
+        />
     </div>
 )
 
